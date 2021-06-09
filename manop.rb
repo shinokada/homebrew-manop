4a47b0d858e11e533789709e8a1567fffcdca5c3
Shinichi Okada <okada.shin@gmail.com>
2021-06-09 15:39:15 +0900

    version 0.1.6

--- manop.rb
+++ manop.rb
@@ -1,8 +1,8 @@
 class Manop < Formula
     desc "Manop outputs a command description or an option description from the man or help page."
     homepage "https://github.com/shinokada/manop"
-    url "https://github.com/shinokada/manop/archive/refs/tags/v0.1.5.tar.gz"
-    sha256 "42fe7b2f9becbf167dcca2067685379945e6d0bb992926434ac3271989fa3bf1"
+    url "https://github.com/shinokada/manop/archive/refs/tags/v0.1.6.tar.gz"
+    sha256 "f928da458d66d4ab6902a4347d044487aaee1910fcddc4a6dd2c2902ac15b214"
     license "MIT"
   
     depends_on "bash" 
